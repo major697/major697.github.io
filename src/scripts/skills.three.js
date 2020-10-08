62061fdd85615cfd8329dea39705e6f9f662a360
Michal Glowacki <michal.glowacki@nowatel.com>
2020-10-08 18:55:52 +0200

    dziala okrag z textem

--- src/scripts/skills.three.js
+++ src/scripts/skills.three.js
@@ -1,79 +1,109 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import font from '@/assets/scss/basic/fonts/gentilis_regular.json'
 
 const skillsThreeJs = (box, data) => {
-  console.log(data)
-  let scene, camera, renderer, startSkills, startMaterial
-  // var color = new THREE.Color('hsl(343, 85%, 50%)')
+  let scene, camera, renderer
 
+  const radius = 50
   scene = new THREE.Scene()
   camera = new THREE.PerspectiveCamera(
     120,
     box.clientWidth / box.clientHeight,
-    0.1,
+    1,
     1000,
   )
-  camera.position.set(0, 0, 100)
-
   renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setSize(box.clientWidth, box.clientHeight)
+
+  camera.position.set(0, 0, 100)
+
+  const controls = new OrbitControls(camera, renderer.domElement)
+  controls.minDistance = 1
+  controls.maxDistance = 1000
+  controls.update()
+
+  // const createBall = () => {
+  //   const geometry = new THREE.SphereBufferGeometry(50, 32, 32)
+  //   const material = new THREE.MeshNormalMaterial({
+  //     wireframe: true,
+  //   })
+  //   // const material = new THREE.MeshLambertMaterial({
+  //   //   transparent: true,
+  //   //   opacity: 0,
+  //   // })
+  //   return new THREE.Mesh(geometry, material)
+  // }
+
+  const random = (min, max) =>
+    Math.floor(Math.random() * (max - min)) + min
+
+  const textPosX = y => {
+    const count = Math.sqrt(Math.pow(radius, 2) - Math.pow(y, 2))
+    return Math.random() < 0.5 ? count * -1 : count
+  }
 
   const createText = text => {
     const setFont = new THREE.Font(font)
 
-    var textGeo = new THREE.TextGeometry(text, {
+    const geometry = new THREE.TextGeometry(text, {
       font: setFont,
-      size: 15,
+      size: 7,
       height: 1,
-      curveSegments: 1,
+      curveSegments: 0.2,
+      color: 0x000000,
       bevelEnabled: true,
-      bevelThickness: 1,
-      bevelSize: 0.3,
-      bevelOffset: 0.5,
-      bevelSegments: 1,
+      bevelThickness: 0.1,
+      bevelSize: 0.1,
+      bevelOffset: 0,
+      bevelSegments: 0.5,
     })
 
-    return textGeo
+    const letterMaterial = new THREE.MeshPhongMaterial({
+      color: 0xff0000,
+    })
 
-    // var textMaterial = new THREE.MeshPhongMaterial({
-    //   color: 0xff0000,
-    // })
-    // return new THREE.Mesh(textGeo, textMaterial)
+    const mesh = new THREE.Mesh(geometry, letterMaterial)
+
+    const offset = new THREE.Object3D()
+    const y = random(-radius, radius)
+
+    offset.add(mesh)
+    lettersBase.add(offset)
+
+    mesh.position.x = textPosX(y)
+    mesh.position.y = y
+    mesh.position.z = random(-radius, radius)
+
+    const geo = new THREE.SphereBufferGeometry(radius, 32, 32)
+    const mat = new THREE.MeshPhongMaterial({
+      wireframe: true,
+    })
+    return new THREE.Mesh(geo, mat)
   }
 
-  const createPointLight = color => {
-    const light = new THREE.PointLight(color, 1)
-    light.position.set(0, 0, 20)
-    return light
+  const createPointLight = () => {
+    return new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6)
   }
 
-  const l1 = createPointLight(0xffffff)
-  //   const skill = createText('JavaScript')
-  //   skill.position.set(0, 0, 0)
+  const lettersBase = new THREE.Object3D()
+  const l1 = createPointLight()
+  const skillText1 = data.map(text => createText(text))
 
-  startSkills = new THREE.Geometry()
-  for (let i = 0; i < data.length; i++) {
-    const skill = new THREE.Vector3(
-      Math.random() * 600 - 300,
-      Math.random() * 600 - 300,
-      Math.random() * 600 - 300,
-    )
-    startSkills.vertices.push(skill)
-  }
-  const getSkill = createText('cos')
-  console.log(getSkill)
-  startMaterial = new THREE.PointsMaterial({
-    color: 0xaaaaaa,
-    size: 40,
-    // map: getSkill,
-  })
+  lettersBase.add(...skillText1)
 
-  const setSkill = new THREE.Points(startSkills, startMaterial)
-  scene.add(setSkill, l1)
+  scene.add(lettersBase, l1)
 
+  // animate
+  console.log(lettersBase)
   const animate = () => {
     requestAnimationFrame(animate)
-    // skill.rotation.x += 0.01
+
+    // lettersBase.children.forEach(text => {
+    //   text.rotation.y += -0.01
+    // })
+
+    controls.update()
     renderer.render(scene, camera)
   }
 
